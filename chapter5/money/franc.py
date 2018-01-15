b36f214bcaba9ad510fe37d42a4d4c7db8139d0b
tkazusa <takekazusa@gmail.com>
2018-01-15 07:00:22 +0000

    add chapter5

---
+++ chapter5/money/franc.py
@@ -0,0 +1,11 @@
+class Franc(object):
+    """ franc class"""
+
+    def __init__(self, amount: int):
+        self._amount = amount
+
+    def times(self, multiplier: int):
+        return Franc(self._amount * multiplier)
+
+    def __eq__(self, other):
+        return self._amount == other._amount
