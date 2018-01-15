c51ea278d64957433d18d0ef3e9dd5b72a4c8546
tkazusa <takekazusa@gmail.com>
2018-01-15 08:26:17 +0000

    add chapter6

---
+++ chapter6/moneys/money.py
@@ -0,0 +1,9 @@
+class Money(object):
+    """Money class"""
+    def __init__(self, amount: int):
+        self._amount = amount
+
+    def __eq__(self, other):
+        return self._amount == other._amount
+
+
---
+++ chapter6/tests/test_money.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+#
+# Author: taketoshi.kazusa
+#
+import sys,os
+sys.path.append(os.pardir)
+
+import pytest
+from moneys.dollar import Dollar
+from moneys.franc import Franc
+
+
+class TestMoney(object):
+    """Test for money class"""
+    
+  
+    @pytest.fixture
+    def dollar(request):
+        return Dollar
+    
+    @pytest.fixture
+    def franc(request):
+        return Franc
+ 
+    def test_DollarMultiplication(self, dollar):
+        assert dollar(5).times(2) == dollar(10)
+        assert dollar(5).times(3) == dollar(15)
+
+    def test_FrancMultiplication(self, franc):
+        assert franc(5).times(2) == franc(10)
+        assert franc(5).times(3) == franc(15)
+
+    def test_Equality(self, dollar):
+        dollar1 = dollar(5)
+        dollar2 = dollar(5)
+        assert dollar1._amount == dollar2._amount
+
+
+        dollar1 = dollar(5)
+        dollar2 = dollar(6)
+        assert dollar1._amount != dollar2._amount
+
+
+
+
+
+        
